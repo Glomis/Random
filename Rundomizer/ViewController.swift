ed6c2ac3ba734f9dbe4608241e05121560f96693
MAx <max@max.ru>
2019-07-29 22:47:27 +0300

    Initial Commit

---
+++ Rundomizer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Rundomizer
+//
+//  Created by Max on 29/07/2019.
+//  Copyright © 2019 Max. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
